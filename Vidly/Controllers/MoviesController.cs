1ae6e95486fc3054dbcd1ab10ee2488d27186103
Saurabh <Saurabh@RSSPL48>
2017-02-06 16:17:55 +0530

    exercise section 2 solution

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,6 +15,8 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek" };
 
+
+
             var customers = new List<Customer>
             {
                 new Customer { Name="cust 1" },
@@ -30,20 +32,35 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         public ActionResult Edit(int id)
         {
             return Content("id= " + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("PageIndex={0}&SortBy={1}", pageIndex, sortBy));
-        }
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("PageIndex={0}&SortBy={1}", pageIndex, sortBy));
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
