7f4c3ad8248a7faaa511e90dc77cc52743b559b9
Saurabh <Saurabh@RSSPL48>
2017-02-06 12:49:37 +0530

    css cahnges ,view added and movies controller added

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex ,string sortBy)
+        {
+            if (!pageIndex.HasValue) 
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("PageIndex={0}&SortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
