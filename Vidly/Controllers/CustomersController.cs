dcdbfc76f40f8107ed5c499966529a7d9272eb77
Saurabh <Saurabh@RSSPL48>
2017-02-07 17:22:32 +0530

    saving customer to DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -49,6 +49,12 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-       
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
