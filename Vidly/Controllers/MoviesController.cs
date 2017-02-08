4c0fc44a54b99cefd133b433364f3a85ab81c259
Saurabh <Saurabh@RSSPL48>
2017-02-08 12:57:54 +0530

    Validation for customer name

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -55,10 +55,7 @@ namespace Vidly.Controllers
                 new Movie { Id = 2, Name = "Wall-e" }
             };
         }
-        public ActionResult Edit(int id)
-        {
-            return Content("id= " + id);
-        }
+
 
         public ActionResult Details(int id)
         {
@@ -84,5 +81,65 @@ namespace Vidly.Controllers
         {
             return Content(year + "/" + month);
         }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            try
+            {
+                               
+                if (movie.Id == 0)
+                {
+                    movie.DateAdded = DateTime.Now;
+                    _context.Movies.Add(movie);
+                }
+                else
+                {
+                    var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                    movieInDb.Name = movie.Name;
+                    movieInDb.GenreId = movie.GenreId;
+                    movieInDb.NumberInStock = movie.NumberInStock;
+                    movieInDb.ReleaseDate = movie.ReleaseDate;
+                }
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Movies");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
