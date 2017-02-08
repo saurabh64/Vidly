e43ce28de60606830b6f460a18b1aea9fc4c2ba6
Saurabh <Saurabh@RSSPL48>
2017-02-08 14:13:30 +0530

    custom error message

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,7 +9,7 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
 
