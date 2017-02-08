0b9d9e58d9e98ebfb3813c25ad0c7ec4fad6775a
Saurabh <Saurabh@RSSPL48>
2017-02-08 15:44:51 +0530

    CSRF

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -51,6 +51,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
